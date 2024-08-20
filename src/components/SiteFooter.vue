6f1f34c498b035890add9c8f599cbe78d4759c7f
mattkauttu <m.a.kauttu95@gmail.com>
2024-08-19 23:54:23 -0400

    Invert shadow to make footer distinct and set footer text to grow for large screens

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,8 +1,8 @@
 <template>
-  <footer class="shadow-md py-4 px-6 flex flex-col md:flex-row items-center justify-between">
+  <footer class="shadow-[0_-4px_6px_-1px_rgb(0_0_0_/_0.1),_0_-2px_4px_-2px_rgb(0_0_0_/_0.1)] py-4 px-6 flex flex-col md:flex-row items-center justify-between">
     <!-- Contact Information Display -->
     <div class="mb-4 md:mb-0">
-      <p class="text-lg">
+      <p class="text-md lg:text-xl">
         If interested, please contact Elizabeth at
         <a href="mailto:elizabethszerba@gmail.com" class="text-blue-400 hover:text-blue-300" aria-label="Email elizabethszerba@gmail.com">elizabethszerba@gmail.com</a>
         or call at
